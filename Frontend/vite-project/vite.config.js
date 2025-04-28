a934e10f87ec92be97d6eb17e43ac8271a829d24
Muhammad Zubair <151429872+LunarLogician@users.noreply.github.com>
2025-04-29 03:53:12 +0500

    Update vite.config.js

--- Frontend/vite-project/vite.config.js
+++ Frontend/vite-project/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SigCoins/',
+  base: '/',
   build: {
     rollupOptions: {
       external: ['react-router-dom', 'axios'],  // Externalize both react-router-dom and axios
